af0783b2ace0741025ff97a75d9d4b5fe528cdd3
Luciano <lucianoalmeida.dev@gmail.com>
2025-05-11 20:38:51 -0300

    Implements FromString in Card struct to parse a card from a code string

--- core/card.go
+++ core/card.go
@@ -1,9 +1,17 @@
 package core
 
+import "errors"
+
 type Card struct {
 	Color Color
 	Value Value
 }
+
+var (
+	ErrInvalidColor    = errors.New("invalid color")
+	ErrInvalidValue    = errors.New("invalid value")
+	ErrInvalidCardCode = errors.New("invalid card code")
+)
 
 func (c *Card) IsWild() bool {
 	return c.Color == CWild || c.Value == VWild || c.Value == VWildDrawFour
@@ -23,6 +31,73 @@ func (c *Card) CanPlayOn(other Card) bool {
 
 func (c *Card) String() string {
 	return c.Color.String() + c.Value.String()
+}
+
+func (c *Card) FromString(code string) error {
+	if len(code) < 2 {
+		return ErrInvalidCardCode
+	}
+
+	colorLetter := code[0:1]
+	valueLetter := code[1:2]
+
+	var color Color
+	var value Value
+	// determines the color
+	switch colorLetter {
+	case "B":
+		color = CBlue
+	case "G":
+		color = CGreen
+	case "R":
+		color = CRed
+	case "Y":
+		color = CYellow
+	case "W":
+		color = CWild
+	default:
+		return ErrInvalidColor
+	}
+
+	switch valueLetter {
+	case "0":
+		value = VZero
+	case "1":
+		value = VOne
+	case "2":
+		value = VTwo
+	case "3":
+		value = VThree
+	case "4":
+		value = VFour
+	case "5":
+		value = VFive
+	case "6":
+		value = VSix
+	case "7":
+		value = VSeven
+	case "8":
+		value = VEight
+	case "9":
+		value = VNine
+	case "S":
+		value = VSkip
+	case "R":
+		value = VReverse
+	case "T":
+		value = VDrawTwo
+	case "W":
+		value = VWild
+	case "F":
+		value = VWildDrawFour
+	default:
+		return ErrInvalidValue
+	}
+
+	c.Color = color
+	c.Value = value
+
+	return nil
 }
 
 type Color int
